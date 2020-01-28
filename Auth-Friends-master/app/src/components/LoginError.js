949b3c1ace545f8ce2a7540dcfdedc49af221135
Mickey R. Groff <mick.groff@gmail.com>
2020-01-28 13:45:25 -0700

    Add files via upload

---
+++ Auth-Friends-master/app/src/components/LoginError.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import unlocked from '../img/unlocked.png';
+
+
+const LoginError = () => {
+
+    return (
+        <div style ={{position: `relative`, top: `200px`}}>
+            <h2>Inturder Step Back Or Die: Fire Cannons</h2>
+            <img style = {{width: `50px`}} src={unlocked}/>
+        </div>
+    )
+}
+
+export default LoginError;
